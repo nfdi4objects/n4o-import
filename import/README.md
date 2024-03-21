c0d4b0ca87c691db1bd84f3f80186af49514ee82
Jakob Voss <voss@gbv.de>
2024-03-21 15:50:32 +0100

    Rename logfile

--- import/README.md
+++ import/README.md
@@ -1,6 +1,3 @@
 Dieses Verzeichnis enthält geprüfte RDF-Daten zugeordnet zu [ihrer jeweiligen
-Sammlung](../n4o-collections.csv), insbesondere die Dateien:
-
-- `absolute.nt` syntaktisch geprüfte und bereinige RDF-Daten
-- `receive.log` Logfile der letzten Prüfung mit Statistiken
-
+Sammlung](../n4o-collections.csv). Die zur Weiterverarbeitung nutzbare RDF-Datei
+mit geprüften Daten ist `absolute.nt`.
--- receive.sh
+++ receive.sh
@@ -1,40 +1,44 @@
 #!/usr/bin/bash
 set -euo pipefail
 
+collection=$1
+input=$2
+
 if [[ $# -ne 2 ]]; then
   echo "Usage: $0 COLLECTION_ID INPUT_FILE"
   echo
-  if [[ $# -eq 1 ]]; then
-    grep -e "^$1," n4o-collections.csv
-  else
-    awk -F, 'NR>1{print $1,$2}' n4o-collections.csv
-  fi
-  exit
 fi
-
-collection=$1
-input=$2
 
 if [[ ! "$collection" =~ ^[0-9]*$ ]]; then
   echo "COLLECTION_ID muss numerisch sein!"
   exit 1
 fi
 
+if [[ $# -eq 1 ]]; then
+  grep -e "^$collection," n4o-collections.csv
+fi
+
+[[ $# -eq 2 ]] || exit 1
+
+
+name=Testammlung
+
 if [[ $collection -eq "0" ]]; then
-  echo "0 = Testcollection"
+  echo "0 = $name"
 else
   name=$(awk -F, "\$1 == $collection {print \$2}" n4o-collections.csv)
   if [[ -z "$name" ]]; then
     echo "COLLECTION_ID $collection unbekannt!"
     exit 1
   fi
-  echo "$collection = $name"
 fi
 
 dir=import/$collection
 
 receive() {
-    echo "Empfange RDF-Daten aus im Turtle-Format aus $input"
+    echo "## $collection: $name"
+    echo
+    echo "Empfangene RDF-Daten im Turtle-Format aus \`$input\` "
 
     original=$dir/original.ttl
     unique=$dir/unique.nt
@@ -44,7 +48,7 @@ receive() {
 
     if [[ ! -s "$tmp" ]]; then
         rm "$tmp"
-        echo "RDF-Daten sind syntaktisch nicht korrekt oder leer!"
+        echo "sind syntaktisch nicht korrekt oder leer!"
         exit 1
     fi
 
@@ -52,20 +56,21 @@ receive() {
     mkdir -p $dir
 
     mv "$tmp" "$unique"
-    echo "Datei ist syntaktisch korrektes RDF"
-    echo "Anzahl unterschiedlicher Tripel: $(wc -l $unique)"
+    echo "ist syntaktisch korrektes RDF. "
+    echo
+    echo "Anzahl unterschiedlicher Tripel: **$(<$unique wc -l)**"
+    echo
 
     # Relative URIs entfernen
 
     absolute=$dir/absolute.nt
 
-    echo
     <$unique awk '$1 !~ /^<file:/ && $2 !~ /<file:/ && $3 !~ /<file:/ { print }' > $absolute
     a=$(<$unique wc -l)
     b=$(<$absolute wc -l)
     removed=$(($a-$b))
 
-    echo "RDF beschränkt auf absolute URIs in $absolute"
+    echo "RDF beschränkt auf absolute URIs in \`$absolute\`. "
     if [[ $removed -ne "0" ]]; then
       echo "$removed triples mit relativen URIs entfernt!"
     fi
@@ -74,11 +79,12 @@ receive() {
 
     properties=$dir/properties
     echo
-    <$absolute awk '{print $2}' | sort | uniq -c | sort -nrk1 > $properties
-    echo "Statistik der Properties: $properties"
-    echo "Anzahl verschiedener Properties:" `<$properties wc -l`
+    <$absolute awk '{print $2}' | sed 's/[<>]//g' | sort | uniq -c | sort -nrk1 > $properties
+    echo "Statistik der Properties in \`$properties\` mit $(<$properties wc -l) Properties."
+    echo "~~~"
     head -3 $properties
     echo "..."
+    echo "~~~"
 
     namespaces=$dir/namespaces
     echo
@@ -86,13 +92,13 @@ receive() {
     <$absolute awk '{print $1} $3~/^</ {print $3}' | sed 's/^<//' | \
         sed 's/#.*$/#/;t;s|/[^/]*>$|/|;t;s/:.*$/:/' | \
         sort | uniq -c | sort -nrk1 > $namespaces
-    echo "Statistik der Namensräume (nur Subjekte und Objekte): $namespaces"
-    echo "Anzahl verschiedener Namensräume:" `<$namespaces wc -l`
-    echo "Bekannte Namensräume:"
+    echo "Statistik der Namensräume (nur Subjekte und Objekte) in \`$namespaces\` mit $(<$namespaces wc -l) Einträgen. "
+    echo "Davon bekannte Namensräume:"
+    echo "~~~"
     <$namespaces ./known-namespaces.py
+    echo "~~~"
 }
 
-receive 2>&1 | tee tmp.log || true
-date  --rfc-3339=seconds > $dir/receive.log
-cat tmp.log >> $dir/receive.log
-rm tmp.log 
+receive 2>&1 | tee tmp.md || true
+mv tmp.md $dir/README.md
+
